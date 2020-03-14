7932103e4585bd0363402e1ce8d320c189b8c08d
Corey <corey@Coreys-MacBook-Pro.local>
2020-03-14 21:43:37 +0800

    Brew installation

--- before_installation.sh
+++ before_installation.sh
@@ -1,5 +1,6 @@
 # Install homebrew
-if [ -n command -v brew 1>/dev/null 2>&1 ]; then
+which -s brew
+if [[ $? != 0 ]]; then
     echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
