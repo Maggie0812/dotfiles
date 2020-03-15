8307f507731f090f82e669474ed677fb57c5db9f
Corey Feng <corey@xfers.com>
2020-03-15 23:06:26 +0800

    Unify fzf path

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,14 +1,15 @@
 # TODO Unify fzf path
+FZF_PATH=$(brew --prefix fzf)
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */$FZF_PATH/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/$FZF_PATH/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/$FZF_PATH/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "/$FZF_PATH/shell/key-bindings.zsh"
