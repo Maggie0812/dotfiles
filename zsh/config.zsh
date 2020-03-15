dd9602c7b7fdb95a2f02f5da81bda0cf47817d9c
Corey Feng <corey@xfers.com>
2020-03-15 16:10:37 +0800

    Use theme p10k

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,18 +5,15 @@ plugins=(git extract brew autojump)
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME=""
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 source $ZSH/oh-my-zsh.sh
 
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-# Pure theme
-fpath+=$HOME/.zsh/pure
-autoload -U promptinit; promptinit
-prompt pure
-PURE_PROMPT_SYMBOL="$ "
+# p10k settings
+[[ ! -f ~/.p10k.zsh ]] || source $DOTFILES/zsh/.p10k.zsh
 
 # FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
